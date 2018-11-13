62b909e375285e81b22b18b4f0c1763ee9b57659
Zach Gibson <zach.gibson@taillight.com>
2018-11-13 13:09:31 -0600

    Turn app sandbox back on and retrieve en0 address via ifaddrs.h and arpa/inet.h

--- LocalToIP/AppDelegate.m
+++ LocalToIP/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 #import "ViewController.h"
+#import <ifaddrs.h>
+#import <arpa/inet.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +18,6 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
     NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
     statusItem = [statusBar statusItemWithLength:NSSquareStatusItemLength];
     
@@ -35,20 +36,34 @@
     [popover setDelegate:self];
     popover.contentViewController = viewController;
     
-    
-    
     ViewController *vc = [[ViewController alloc] init];
     ipAddress = self.getIPAddress;
     [vc setIP:ipAddress];
 }
 
 - (NSString *)getIPAddress {
-    NSString *source = @"do shell script \"ipconfig getifaddr en0\"";
-    NSAppleScript *script = [[NSAppleScript alloc] initWithSource:source];
-    NSDictionary *dict = nil;
-    NSAppleEventDescriptor *descriptor = [script executeAndReturnError:&dict];
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
     
-    return [descriptor stringValue];
+    return address;
 }
 
 - (void)togglePopover:(id)sender {
